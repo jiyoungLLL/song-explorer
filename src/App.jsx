473aaf5c7d9dc0646427f0991a9909352b25ec59
[AI10] 임지영 <dlawldud021@naver.com>
2024-11-25 22:00:17 +0900

    React-router 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
+import LikedPage from './pages/LikedPage';
 import SearchPage from './pages/SearchPage';
 
 const queryClient = new QueryClient(); // 사용할 객체
 
 function App() {
   return (
-    <Container>
-      <QueryClientProvider client={queryClient}>
-        {/* 내부에서 React-query 사용 가능 */}
-        <SearchPage />
-      </QueryClientProvider>
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <QueryClientProvider client={queryClient}>
+          {/* 내부에서 React-query 사용 가능 */}
+          {/* <SearchPage /> */}
+          <Routes>
+            <Route path='/' element={<SearchPage />} />
+            <Route path='/liked' element={<LikedPage />} />
+          </Routes>
+        </QueryClientProvider>
+      </Container>
+    </BrowserRouter>
   );
 }
 
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,11 +1,19 @@
 import SearchBar from '../../components/SearchBar';
 import SongList from '../../components/SongList'; // 디렉토리 내에서 index.jsx라고 정의했기 때문
 // .jsx 확장자를 생각하는 이유는 번들러 때문
+import { Link, useNavigate } from 'react-router';
 
 function SearchPage() {
+  let navigate = useNavigate();
   return (
     <div>
       {/* 의존성 문제 해결 */}
+      <Link to='/liked'>Liked</Link>
+      <button
+        onClick={() => {
+          navigate('/liked');
+        }}
+      />
       <SearchBar />
       <SongList />
     </div>
